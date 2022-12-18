1e152eb728af3c815fb615d465c13ad70be76461
Martin Goldman <mgoldman@skytap.com>
2022-12-18 00:08:35 -0500

    make sinatra endpoints more dynamic

--- script-templates/lib/metadata_stub_server/server.rb
+++ script-templates/lib/metadata_stub_server/server.rb
@@ -1,12 +1,15 @@
 require "sinatra/base"
-require "pry"
+
 class MetadataStubServer < Sinatra::Base
-  attr_accessor :control_data_json, :metadata_json, :control_url
+  attr_accessor :control_data_json, :metadata_json, :lab_url, :control_url, :broadcast_url
 
   def initialize(*args)
     self.control_data_json = File.read(File.join(File.dirname(__FILE__), "control_data.json"))
     self.metadata_json = File.read(File.join(File.dirname(__FILE__), "metadata.json.erb"))
-    self.control_url = ENV['CONTROL_URL'] || "http://gw/lab_access/self_learner/1/111/control/1/111"
+
+    self.lab_url = ENV['CM_LAB_URL'] || "http://gw/lab_access/self_learner/1/111"
+    self.control_url = ENV['CM_CONTROL_URL'] || URI.join(lab_url, "control/1/111").to_s
+    self.broadcast_url = ENV['CM_BROADCAST_URL'] || URI.join(lab_url, "learning_console/broadcast").to_s
     super
   end
 
@@ -16,11 +19,11 @@ class MetadataStubServer < Sinatra::Base
     ERB.new(metadata_json).result(binding)
   end
 
-  get "/lab_access/self_learner/1/111/control/1/111" do
+  get control_url do
     control_data_json
   end
 
-  put "/lab_access/self_learner/1/111/control/1/111" do
+  put control_url do
     body = JSON.parse(request.body.read) rescue {}
     if integration_data = body["integration_data"]
       self.control_data_json = JSON.parse(control_data_json).tap do |h|
@@ -31,7 +34,7 @@ class MetadataStubServer < Sinatra::Base
     control_data_json
   end
 
-  post "/lab_access/self_learner/1/111/learning_console/broadcast" do
+  post broadcast_url do
     "{}"
   end
 end
