9d9bebccbeced3c3bc4388221f0311dbce964387
Martin Goldman <mgoldman@skytap.com>
2022-12-19 00:28:17 -0500

    bash template, ruby fixes, metadata stub server fixes

--- script-templates/lib/metadata_stub_server/server.rb
+++ script-templates/lib/metadata_stub_server/server.rb
@@ -15,40 +15,36 @@
 require "sinatra/base"
 
 class MetadataStubServer < Sinatra::Base
-  attr_accessor :control_data_json, :metadata_json, :lab_url, :control_url, :broadcast_url
+  set :lab_url, ENV['CM_LAB_URL'] || "http://gw/lab_access/self_learner/1/111"
+  set :control_url, ENV['CM_CONTROL_URL'] || "#{settings.lab_url}/control/1/111"
+  set :broadcast_url, ENV['CM_BROADCAST_URL'] || "#{settings.lab_url}/learning_console/broadcast"
 
-  def initialize(*args)
-    self.control_data_json = File.read(File.join(File.dirname(__FILE__), "control_data.json"))
-    self.metadata_json = File.read(File.join(File.dirname(__FILE__), "metadata.json.erb"))
-
-    self.lab_url = ENV['CM_LAB_URL'] || "http://gw/lab_access/self_learner/1/111"
-    self.control_url = ENV['CM_CONTROL_URL'] || URI.join(lab_url, "control/1/111").to_s
-    self.broadcast_url = ENV['CM_BROADCAST_URL'] || URI.join(lab_url, "learning_console/broadcast").to_s
-    super
-  end
+  set :control_data_json, File.read(File.join(File.dirname(__FILE__), "control_data.json"))
+  set :metadata_json, File.read(File.join(File.dirname(__FILE__), "metadata.json.erb"))
 
   before { content_type "application/json" }
 
   get '/skytap' do
-    ERB.new(metadata_json).result(binding)
+    control_url = settings.control_url
+    ERB.new(settings.metadata_json).result(binding)
   end
 
-  get control_url do
-    control_data_json
+  get URI(settings.control_url).path do
+    settings.control_data_json
   end
 
-  put control_url do
+  put URI(settings.control_url).path do
     body = JSON.parse(request.body.read) rescue {}
     if integration_data = body["integration_data"]
-      self.control_data_json = JSON.parse(control_data_json).tap do |h|
+      settings.control_data_json = JSON.parse(settings.control_data_json).tap do |h|
         h["integration_data"] = integration_data
       end.to_json
     end
 
-    control_data_json
+    settings.control_data_json
   end
 
-  post broadcast_url do
+  post URI(settings.broadcast_url).path do
     "{}"
   end
 end
